6219cf9841a788575b216aeeffc89b67d282b924
taqiyyahnabiilah@gmail.com <taqiyyahnabiilah@gmail.com>
2026-04-15 12:23:25 +0700

    qiya

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 use App\Http\Controllers\databarang_controller;
 use App\Http\Controllers\databarang1_controller;
+use App\Http\Controllers\databarang2_controller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +10,4 @@ Route::get('/', function () {
 
 Route::get('/barang', [databarang_controller::class, 'tampilkan']);
 Route::get('/barang1', [databarang1_controller::class, 'tampilkan']);
+Route::get('/barang2', [databarang2_controller::class, 'tampilkan']);
