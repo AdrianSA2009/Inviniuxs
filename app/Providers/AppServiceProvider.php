ef2363bd5ee43ed9ef104991700a5b3e7d04fc34
adrian <adriansa0125@gmail.com>
2026-06-23 00:05:41 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    
     /**
      * Register any application services.
      */
